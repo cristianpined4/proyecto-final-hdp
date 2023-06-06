6c4e1a3855646df141b14722c43b2c0aafae3544
Cristian Pineda <cristian.pineda.1997@outlook.com>
2023-06-05 20:19:06 -0600

    fix bugs in login pages

--- controllers/Login.js
+++ controllers/Login.js
@@ -36,33 +36,37 @@ document.addEventListener("submit", (e) => {
         e.target[i].parentElement.appendChild(error);
         return;
       }
-      let err = document.querySelector(".error.form-message");
-      if (err != null) {
-        err.parentElement.removeChild(err);
+      let error = e.target[i].parentElement.querySelector(".error");
+      if (error != null) {
+        e.target[i].parentElement.removeChild(error);
       }
-      let usuario = new Usuarios();
-      let exist = usuario.all().find((el) => el.username == data.username);
-      if (exist == undefined) {
-        err = document.createElement("p");
-        err.classList.add("error", "form-message");
-        err.innerHTML = `<br>El usuario <b>${data.username}</b> no existe.`;
-        e.target.appendChild(err);
-        return;
-      }
-      if (exist.password != data.password) {
-        err = document.createElement("p");
-        err.classList.add("error", "form-message");
-        err.innerHTML = `<br>La contraseña es incorrecta.`;
-        e.target.appendChild(err);
-        return;
-      }
-      localStorage.setItem("current-user", JSON.stringify(exist));
-      let route = new Router();
-      if (exist.rol == "admin") {
-        route.GoTo("?view=Admin");
-        return;
-      }
-      route.GoTo("?view=Inicio");
     }
+    let err = document.querySelector(".error.form-message");
+    if (err != null) {
+      err.parentElement.removeChild(err);
+    }
+    let usuario = new Usuarios();
+    let exist = usuario.all().find((el) => el.username == data.username);
+    if (exist == undefined) {
+      err = document.createElement("p");
+      err.classList.add("error", "form-message");
+      err.innerHTML = `<br>El usuario <b>${data.username}</b> no existe.`;
+      e.target.appendChild(err);
+      return;
+    }
+    if (exist.password != data.password) {
+      err = document.createElement("p");
+      err.classList.add("error", "form-message");
+      err.innerHTML = `<br>La contraseña es incorrecta.`;
+      e.target.appendChild(err);
+      return;
+    }
+    localStorage.setItem("current-user", JSON.stringify(exist));
+    let route = new Router();
+    if (exist.rol == "admin") {
+      route.GoTo("?view=Admin");
+      return;
+    }
+    route.GoTo("?view=Inicio");
   }
 });
